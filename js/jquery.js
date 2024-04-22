5fca02e9a1deb7293995dcfedca5124b9a16bd6f
AAROHI DIXIT <148526981+aarohidixit31@users.noreply.github.com>
2024-04-22 13:50:59 +0530

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -148,3 +148,85 @@
         trim: function(a) {
             return null == a ? "" : (a + "").replace(o, "")
         },
+        makeArray: function(a, b) {
+            var c = b || [];
+            return null != a && (s(Object(a)) ? n.merge(c, "string" == typeof a ? [a] : a) : g.call(c, a)),
+            c
+        },
+        inArray: function(a, b, c) {
+            return null == b ? -1 : h.call(b, a, c)
+        },
+        merge: function(a, b) {
+            for (var c = +b.length, d = 0, e = a.length; c > d; d++)
+                a[e++] = b[d];
+            return a.length = e,
+            a
+        },
+        grep: function(a, b, c) {
+            for (var d, e = [], f = 0, g = a.length, h = !c; g > f; f++)
+                d = !b(a[f], f),
+                d !== h && e.push(a[f]);
+            return e
+        },
+        map: function(a, b, c) {
+            var d, e, g = 0, h = [];
+            if (s(a))
+                for (d = a.length; d > g; g++)
+                    e = b(a[g], g, c),
+                    null != e && h.push(e);
+            else
+                for (g in a)
+                    e = b(a[g], g, c),
+                    null != e && h.push(e);
+            return f.apply([], h)
+        },
+        guid: 1,
+        proxy: function(a, b) {
+            var c, d, f;
+            return "string" == typeof b && (c = a[b],
+            b = a,
+            a = c),
+            n.isFunction(a) ? (d = e.call(arguments, 2),
+            f = function() {
+                return a.apply(b || this, d.concat(e.call(arguments)))
+            }
+            ,
+            f.guid = a.guid = a.guid || n.guid++,
+            f) : void 0
+        },
+        now: Date.now,
+        support: l
+    }),
+    "function" == typeof Symbol && (n.fn[Symbol.iterator] = c[Symbol.iterator]),
+    n.each("Boolean Number String Function Array Date RegExp Object Error Symbol".split(" "), function(a, b) {
+        i["[object " + b + "]"] = b.toLowerCase()
+    });
+    function s(a) {
+        var b = !!a && "length"in a && a.length
+          , c = n.type(a);
+        return "function" === c || n.isWindow(a) ? !1 : "array" === c || 0 === b || "number" == typeof b && b > 0 && b - 1 in a
+    }
+    var t = function(a) {
+        var b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u = "sizzle" + 1 * new Date, v = a.document, w = 0, x = 0, y = ga(), z = ga(), A = ga(), B = function(a, b) {
+            return a === b && (l = !0),
+            0
+        }, C = 1 << 31, D = {}.hasOwnProperty, E = [], F = E.pop, G = E.push, H = E.push, I = E.slice, J = function(a, b) {
+            for (var c = 0, d = a.length; d > c; c++)
+                if (a[c] === b)
+                    return c;
+            return -1
+        }, K = "checked|selected|async|autofocus|autoplay|controls|defer|disabled|hidden|ismap|loop|multiple|open|readonly|required|scoped", L = "[\\x20\\t\\r\\n\\f]", M = "(?:\\\\.|[\\w-]|[^\\x00-\\xa0])+", N = "\\[" + L + "*(" + M + ")(?:" + L + "*([*^$|!~]?=)" + L + "*(?:'((?:\\\\.|[^\\\\'])*)'|\"((?:\\\\.|[^\\\\\"])*)\"|(" + M + "))|)" + L + "*\\]", O = ":(" + M + ")(?:\\((('((?:\\\\.|[^\\\\'])*)'|\"((?:\\\\.|[^\\\\\"])*)\")|((?:\\\\.|[^\\\\()[\\]]|" + N + ")*)|.*)\\)|)", P = new RegExp(L + "+","g"), Q = new RegExp("^" + L + "+|((?:^|[^\\\\])(?:\\\\.)*)" + L + "+$","g"), R = new RegExp("^" + L + "*," + L + "*"), S = new RegExp("^" + L + "*([>+~]|" + L + ")" + L + "*"), T = new RegExp("=" + L + "*([^\\]'\"]*?)" + L + "*\\]","g"), U = new RegExp(O), V = new RegExp("^" + M + "$"), W = {
+            ID: new RegExp("^#(" + M + ")"),
+            CLASS: new RegExp("^\\.(" + M + ")"),
+            TAG: new RegExp("^(" + M + "|[*])"),
+            ATTR: new RegExp("^" + N),
+            PSEUDO: new RegExp("^" + O),
+            CHILD: new RegExp("^:(only|first|last|nth|nth-last)-(child|of-type)(?:\\(" + L + "*(even|odd|(([+-]|)(\\d*)n|)" + L + "*(?:([+-]|)" + L + "*(\\d+)|))" + L + "*\\)|)","i"),
+            bool: new RegExp("^(?:" + K + ")$","i"),
+            needsContext: new RegExp("^" + L + "*[>+~]|:(even|odd|eq|gt|lt|nth|first|last)(?:\\(" + L + "*((?:-\\d)?\\d*)" + L + "*\\)|)(?=[^-]|$)","i")
+        }, X = /^(?:input|select|textarea|button)$/i, Y = /^h\d$/i, Z = /^[^{]+\{\s*\[native \w/, $ = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/, _ = /[+~]/, aa = /'|\\/g, ba = new RegExp("\\\\([\\da-f]{1,6}" + L + "?|(" + L + ")|.)","ig"), ca = function(a, b, c) {
+            var d = "0x" + b - 65536;
+            return d !== d || c ? b : 0 > d ? String.fromCharCode(d + 65536) : String.fromCharCode(d >> 10 | 55296, 1023 & d | 56320)
+        }, da = function() {
+            m()
+        };
